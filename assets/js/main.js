9fedfab4d07e45d9660f2abc18ecb4111660c358
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-19 17:36:45 +0100

    add third step in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,23 @@ console.log(user_age, user_km);
 
 */
 
+if (user_age < 18) {
+    
+    let total_price = (user_km * 0.21) / 100 * 80;
+    console.log(total_price);
+
+} else if (user_age > 65) {
+
+    let total_price = (user_km * 0.21) / 100 * 60;
+    console.log(total_price);
+
+} else {
+
+    let total_price = (user_km * 0.21);
+    console.log(total_price);
+}
+
+
+
+
 // - creare l'output con le regole sopracitate 
