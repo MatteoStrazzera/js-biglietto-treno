f12f9c1da50f0538314d5a9df9041c3443960874
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-19 18:15:22 +0100

    fix third step and add last step with comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 // - creare le variabili con i valori che l'utente dichiarerà
 // - creare due prompt che permettano all'utente di dichiarare l'età ed i chilometri che dovrà percorrere
 
-const user_age = Number(prompt("Inserisci la tua età"));
+const user_age = Number(prompt("Inserisci la tua età")); // user age variable
 
-const user_km = Number(prompt("Inserisci quanti chilometri percorrerai"));
+const user_km = Number(prompt("Inserisci quanti chilometri percorrerai")); // user km variable
 
 console.log(user_age, user_km);
 /*
@@ -15,23 +15,19 @@ console.log(user_age, user_km);
 
 */
 
+let total_price = user_km * 0.21; // standard price
+
 if (user_age < 18) {
     
-    let total_price = (user_km * 0.21) / 100 * 80;
-    console.log(total_price);
+     total_price = total_price / 100 * 80; // 20% discounted price
+    
 
 } else if (user_age > 65) {
 
-    let total_price = (user_km * 0.21) / 100 * 60;
-    console.log(total_price);
-
-} else {
-
-    let total_price = (user_km * 0.21);
-    console.log(total_price);
+     total_price = total_price / 100 * 60; // 40% discounted price
+    
 }
 
-
-
-
 // - creare l'output con le regole sopracitate 
+
+console.log('Il prezzo del tuo biglietto è: ' + total_price.toFixed(2) + ' \u20AC' ); // total price output
