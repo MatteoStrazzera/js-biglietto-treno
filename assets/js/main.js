1f6649d2ae97e1b6270c849022dea8ef57f197d9
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-19 16:31:21 +0100

    add first and second steps in js

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// - creare le variabili con i valori che l'utente dichiarerà
+// - creare due prompt che permettano all'utente di dichiarare l'età ed i chilometri che dovrà percorrere
+
+const user_age = Number(prompt("Inserisci la tua età"));
+
+const user_km = Number(prompt("Inserisci quanti chilometri percorrerai"));
+
+console.log(user_age, user_km);
+/*
+
+- creare le condizioni per il calcolo del prezzo del biglietto con le seguenti regole:
+  - se minorenne -> sconto del 20%
+  - se over 65 -> sconto del 40%
+  - se non rientra in nessuno dei primi casi il prezzo sarà pieno
+
+*/
+
+// - creare l'output con le regole sopracitate 
